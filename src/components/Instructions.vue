631951df03b078520d8d5f4fd0b648058814b4a2
Alan Turing <freemasl.sf@gmail.com>
2021-08-26 17:05:11 -0600

    Fix spelling error

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -2,7 +2,7 @@
   <section class="instructions">
     <p class="instruction">Pangrams are sentences which use every single letter of the alphabet. ex: “A quick brown fox jumps over the lazy dog.”</p>
     <p class="instruction">Please fork this project and write a function which determines if the input is a pangram or not. If the input is a pangram return "Pangram!" If it is not, 
-    please return the letters of the alphabet that are missing from the sentance. The sentence may include upper or lowercase letters or special characters
+    please return the letters of the alphabet that are missing from the sentence. The sentence may include upper or lowercase letters or special characters
     but the returned letters should all be lowercase with special characters ommitted. You may use any libraries or tools at your disposal to help complete this project.
     Please also include responsive css styles on this page (this paragraph included) and the output as well, the design is up to you (be creative if you like!). Bonus points for validation and including accessibility.</p>
 
